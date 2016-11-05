20326f44eba1577a2907c8a6ee4408bb506ffafc
martinpastore <martinpablopastore@gmail.com>
2016-11-05 12:31:48 -0300

    first commit

---
+++ activeApp/activeApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace activeApp.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ activeApp/activeApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="activeApp.MvcApplication" Language="C#" %>
